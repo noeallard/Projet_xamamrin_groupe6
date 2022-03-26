fe57e16f9e537082d7ba32e646ff1a1f6fdd1910
Yohann <yohann.baudet@etu.univ-orleans.fr>
2022-03-26 13:05:16 +0100

    Ajout des pages de l'ancien projet

---
+++ src/TimeTracker.Apps/ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Storm.Mvvm;
+using TimeTracker.Apps.Pages;
+using Xamarin.Forms;
+
+namespace TimeTracker.Apps.ViewModels
+{
+    public class ConnectionViewModel : ViewModelBase
+    {
+        public async void onClickRegisterButton()
+        {
+            await NavigationService.PushAsync<RegisterPage>();
+        }
+
+        public Command OnClickRegisterButton
+        {
+            get;
+        }
+
+        public ConnectionViewModel()
+        {
+            OnClickRegisterButton = new Command(onClickRegisterButton);
+        }
+    }
+}
---
+++ src/TimeTracker.Apps/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Storm.Mvvm;
+using Storm.Mvvm.Services;
+using TimeTracker.Apps.Pages;
+using Xamarin.Forms;
+
+namespace TimeTracker.Apps.ViewModels
+{
+    internal class RegisterViewModel : ViewModelBase
+
+    {
+        public async void onClick()
+        {
+            await NavigationService.PushAsync<MainPage>();
+        }
+        public Command OnClick
+        {
+            get;
+        }
+        public RegisterViewModel()
+        {
+            OnClick = new Command(onClick);
+        }
+    }
+}
