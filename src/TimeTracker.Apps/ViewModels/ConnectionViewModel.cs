2b235520e33a77301a695110120aeaa9aa86687f
Yohann <yohann.baudet@etu.univ-orleans.fr>
2022-03-26 16:12:11 +0100

    Ajout lecture du login et mdp

--- src/TimeTracker.Apps/ViewModels/ConnectionViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/ConnectionViewModel.cs
@@ -1,11 +1,44 @@
-﻿using Storm.Mvvm;
+﻿using Newtonsoft.Json;
+using Storm.Mvvm;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
 using TimeTracker.Apps.Pages;
+using TimeTracker.Dtos;
+using TimeTracker.Dtos.Authentications.Credentials;
 using Xamarin.Forms;
 
 namespace TimeTracker.Apps.ViewModels
 {
     public class ConnectionViewModel : ViewModelBase
     {
+
+        HttpClient client;
+        private String _login;
+        private String _password;
+
+        public String Login
+        {
+            get { return _login; }
+            set
+            {
+                SetProperty(ref _login, value);
+            }
+        }
+
+        public String Password 
+        { 
+            get { return _password; }
+            set
+            {
+                SetProperty(ref _password, value);
+            }
+        }
+
+
+
+
         public async void onClickRegisterButton()
         {
             await NavigationService.PushAsync<RegisterPage>();
@@ -16,9 +49,41 @@ namespace TimeTracker.Apps.ViewModels
             get;
         }
 
+        public async void onClickConnectionButton()
+        {
+            LoginWithCredentialsRequest loginRequest = new LoginWithCredentialsRequest();
+            loginRequest.ClientId = "MOBILE";
+            loginRequest.ClientSecret = "COURS";
+            loginRequest.Login = _login;
+            loginRequest.Password = _password;
+
+            string json = JsonConvert.SerializeObject(loginRequest, Formatting.Indented);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                Uri uri = new Uri(Urls.HOST + "/" + Urls.LOGIN);
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseBody);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+
+        public Command OnClickConnectionButton
+        {
+            get;
+        }
+
         public ConnectionViewModel()
         {
             OnClickRegisterButton = new Command(onClickRegisterButton);
+            OnClickConnectionButton = new Command(onClickConnectionButton);
+            client = new HttpClient();
         }
     }
 }
