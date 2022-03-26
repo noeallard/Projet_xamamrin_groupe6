ecbd5a8934132d392778138ab7018150513e3aee
Yohann <yohann.baudet@etu.univ-orleans.fr>
2022-03-26 13:51:51 +0100

    Ajout user(pas utilisé) et début requête register

---
+++ src/TimeTracker.Apps/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Apps.Models
+{
+    internal class User
+    {
+        private string email;
+        private string name;
+        private string lastname;
+    }
+}
--- src/TimeTracker.Apps/ViewModels/RegisterViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/RegisterViewModel.cs
@@ -1,16 +1,56 @@
-﻿using Storm.Mvvm;
+﻿using Newtonsoft.Json;
+using Storm.Mvvm;
 using Storm.Mvvm.Services;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
 using TimeTracker.Apps.Pages;
+using TimeTracker.Dtos;
+using TimeTracker.Dtos.Accounts;
 using Xamarin.Forms;
 
 namespace TimeTracker.Apps.ViewModels
 {
     internal class RegisterViewModel : ViewModelBase
-
     {
+
+        HttpClient client;
+
         public async void onClick()
         {
-            await NavigationService.PushAsync<MainPage>();
+            CreateUserRequest registerRequest = new CreateUserRequest();
+            registerRequest.ClientId = "MOBILE";
+            registerRequest.ClientSecret = "COURS";
+
+            registerRequest.Email = "test1@gmail.com";
+            registerRequest.Password = "password";
+            registerRequest.FirstName = "test1";
+            registerRequest.LastName = "test1";
+
+            string json = JsonConvert.SerializeObject(registerRequest, Formatting.Indented);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            
+
+            try
+            {
+                Debug.WriteLine(Urls.HOST+"/"+Urls.CREATE_USER);
+                Uri uri = new Uri(Urls.HOST + "/" + Urls.CREATE_USER);
+                Debug.WriteLine(uri.ToString());
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            
+
+
+            /*await NavigationService.PushAsync<MainPage>();*/
         }
         public Command OnClick
         {
@@ -19,6 +59,7 @@ namespace TimeTracker.Apps.ViewModels
         public RegisterViewModel()
         {
             OnClick = new Command(onClick);
+            client = new HttpClient();
         }
     }
 }
