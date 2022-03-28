a5bd44042f66feb7067dd9e36d076d5c2d9fd4a7
Yohann <yohann.baudet@etu.univ-orleans.fr>
2022-03-28 19:18:36 +0200

    Suppression couleur quand on cliquait sur un projet

--- src/TimeTracker.Apps/Pages/MainPage.xaml.cs
+++ src/TimeTracker.Apps/Pages/MainPage.xaml.cs
@@ -12,6 +12,12 @@ namespace TimeTracker.Apps.Pages
             InitializeComponent();
             BindingContext = new MainViewModel();
             NavigationPage.SetHasBackButton(this, false);
+            mainList.ItemTapped += (object sender, ItemTappedEventArgs e) => {
+                // don't do anything if we just de-selected the row.
+                if (e.Item == null) return;
+                // Deselect the item.
+                if (sender is ListView lv) lv.SelectedItem = null;
+            };
         }
     }
 }
