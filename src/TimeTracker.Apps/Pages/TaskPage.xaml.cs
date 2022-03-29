af2a43078751919c38b49723003e095096228a1e
Noe Allard <o2180328@campus.univ-orleans.fr>
2022-03-29 20:40:12 +0200

    Ajout fichier pas complete taskpage et viewmodel

---
+++ src/TimeTracker.Apps/Pages/TaskPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Apps.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeTracker.Apps.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskPage : ContentPage
+    {
+        public TaskPage()
+        {
+            InitializeComponent();
+            BindingContext = new TaskViewModel();
+        }
+    }
+}
---
+++ src/TimeTracker.Apps/ViewModels/TaskViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using Storm.Mvvm;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using TimeTracker.Dtos;
+using Xamarin.Essentials;
+
+namespace TimeTracker.Apps.ViewModels
+{
+    public class TaskViewModel : ViewModelBase
+    {
+        HttpClient client;
+        public async void loadTask()
+        {
+            try
+            {
+                Uri uri = new Uri(Urls.HOST + "/" + Urls.USER_PROFILE);
+                if (!client.DefaultRequestHeaders.Contains("Authorization"))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Preferences.Get("access_token", "undefiend"));
+                }
+                HttpResponseMessage response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var parsedObject = JObject.Parse(responseBody);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        public TaskViewModel()
+        {
+            
+            client = new HttpClient();
+            loadTask();
+        }
+    }
+}
