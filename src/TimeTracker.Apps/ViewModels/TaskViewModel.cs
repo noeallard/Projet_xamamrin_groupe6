c662220441037328352d29c5354716fe111586e4
Noe Allard <o2180328@campus.univ-orleans.fr>
2022-04-02 19:13:55 +0200

    Ajout modification page task timer pas encore implémenté

--- src/TimeTracker.Apps/ViewModels/TaskViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/TaskViewModel.cs
@@ -6,6 +6,11 @@ using System.Net.Http;
 using TimeTracker.Dtos;
 using TimeTracker.Dtos.Projects;
 using Xamarin.Essentials;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace TimeTracker.Apps.ViewModels
 {
@@ -14,10 +19,13 @@ namespace TimeTracker.Apps.ViewModels
     {
         private TaskItem _task;
         private String _name;
+        private string _nameButton;
         private long _Id;
         private int _projectId;
+        private ObservableCollection<TimeItem> _times;
+        HttpClient client;
 
-       public String Name
+        public String Name
         {
             get => _name;
             set => SetProperty(ref _name, value);
@@ -28,17 +36,83 @@ namespace TimeTracker.Apps.ViewModels
             set => SetProperty(ref _Id, value);
         }
 
+        public ObservableCollection<TimeItem> Times
+        {
+            get => _times;
+            set => SetProperty(ref _times, value);
+        }
+
+        public string NameButton
+        {
+            get => _nameButton;
+            set => SetProperty(ref _nameButton, value);
+        }
         public TaskViewModel(TaskItem task,int projectId)
         {
             _task = task;
             _name = task.Name;
             _Id = task.Id;
             _projectId = projectId;
-
-
-            
-
+            _times = convertList(_task, task.Times);
+            client = new HttpClient();
+            OnClickAddTimeButton = new Command(onClickAddTimeButton);
+            _nameButton = "Démarrer";
         }
 
+        public ObservableCollection<TimeItem> convertList(TaskItem task, List<TimeItem> times)
+        {
+            ObservableCollection<TimeItem> finalList = new ObservableCollection<TimeItem>();
+            foreach(TimeItem time in task.Times)
+            {
+                TimeSpan diff = time.EndTime.Subtract(time.StartTime);
+                time.Difference = diff;
+                Debug.WriteLine(time.Difference);
+                Debug.WriteLine(time.Id);
+                finalList.Add(time);
+            }
+            return finalList;
+        }
+
+        public Command OnClickAddTimeButton
+        {
+            get;
+        }
+
+        public async void onClickAddTimeButton()
+        {
+            AddTimeRequest addTimeRequest = new AddTimeRequest();
+            addTimeRequest.StartTime = DateTime.Now;
+            addTimeRequest.EndTime = DateTime.Now;
+
+            string json = JsonConvert.SerializeObject(addTimeRequest, Formatting.Indented);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                Uri uri = new Uri((Urls.HOST + "/" + Urls.ADD_TIME).Replace("{projectId}",_projectId.ToString()).Replace("{taskId}", _Id.ToString()));
+                if (!client.DefaultRequestHeaders.Contains("Authorization"))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Preferences.Get("access_token", "undefiend"));
+                }
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var parsedObject = JObject.Parse(responseBody);
+                    TimeItem item = JsonConvert.DeserializeObject<TimeItem>(responseBody);
+                    _task.Times.Add(item);
+                    Times = convertList(_task, _task.Times);
+                    Debug.WriteLine(response);
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
     }
 }
--- src/TimeTracker.Dtos/Projects/TimeItem.cs
+++ src/TimeTracker.Dtos/Projects/TimeItem.cs
@@ -5,6 +5,12 @@ namespace TimeTracker.Dtos.Projects
 {
     public class TimeItem
     {
+        public TimeItem(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
         [JsonProperty("id")]
         public long Id { get; set; }
         
@@ -13,5 +19,8 @@ namespace TimeTracker.Dtos.Projects
         
         [JsonProperty("end_time")]
         public DateTime EndTime { get; set; }
+
+        public TimeSpan Difference { get; set; }
+        
     }
 }
