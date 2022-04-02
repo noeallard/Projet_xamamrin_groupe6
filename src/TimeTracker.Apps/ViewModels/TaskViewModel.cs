b4144126fddbc3e6eabe242d3c21a26631e6bac1
Noe Allard <o2180328@campus.univ-orleans.fr>
2022-04-02 22:42:42 +0200

    Timer dans tache fini

--- src/TimeTracker.Apps/ViewModels/TaskViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/TaskViewModel.cs
@@ -20,15 +20,24 @@ namespace TimeTracker.Apps.ViewModels
         private TaskItem _task;
         private String _name;
         private string _nameButton;
+        private string _timer;
         private long _Id;
         private int _projectId;
         private ObservableCollection<TimeItem> _times;
         HttpClient client;
+        private DateTime _startTime;
+        private Boolean _start;
 
         public String Name
         {
             get => _name;
             set => SetProperty(ref _name, value);
+        }
+
+        public Boolean Start
+        {
+            get => _start;
+            set => SetProperty(ref _start, value);
         }
         public long Id
         {
@@ -47,6 +56,12 @@ namespace TimeTracker.Apps.ViewModels
             get => _nameButton;
             set => SetProperty(ref _nameButton, value);
         }
+
+        public string Timer
+        {
+            get => _timer;
+            set => SetProperty(ref _timer, value);
+        }
         public TaskViewModel(TaskItem task,int projectId)
         {
             _task = task;
@@ -57,6 +72,8 @@ namespace TimeTracker.Apps.ViewModels
             client = new HttpClient();
             OnClickAddTimeButton = new Command(onClickAddTimeButton);
             _nameButton = "Démarrer";
+            _start = false;
+            _timer = "";
         }
 
         public ObservableCollection<TimeItem> convertList(TaskItem task, List<TimeItem> times)
@@ -65,7 +82,7 @@ namespace TimeTracker.Apps.ViewModels
             foreach(TimeItem time in task.Times)
             {
                 TimeSpan diff = time.EndTime.Subtract(time.StartTime);
-                time.Difference = diff;
+                time.Difference = new TimeSpan(diff.Hours, diff.Minutes, diff.Seconds);
                 Debug.WriteLine(time.Difference);
                 Debug.WriteLine(time.Id);
                 finalList.Add(time);
@@ -80,39 +97,69 @@ namespace TimeTracker.Apps.ViewModels
 
         public async void onClickAddTimeButton()
         {
-            AddTimeRequest addTimeRequest = new AddTimeRequest();
-            addTimeRequest.StartTime = DateTime.Now;
-            addTimeRequest.EndTime = DateTime.Now;
-
-            string json = JsonConvert.SerializeObject(addTimeRequest, Formatting.Indented);
-            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            try
+            if (NameButton == "Démarrer")
             {
-                Uri uri = new Uri((Urls.HOST + "/" + Urls.ADD_TIME).Replace("{projectId}",_projectId.ToString()).Replace("{taskId}", _Id.ToString()));
-                if (!client.DefaultRequestHeaders.Contains("Authorization"))
+                _start = true;
+                NameButton = "Stop";
+                _startTime = DateTime.Now;
+                Device.StartTimer(new TimeSpan(0, 0, 1), () =>
                 {
-                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Preferences.Get("access_token", "undefiend"));
-                }
-                HttpResponseMessage response = await client.PostAsync(uri, content);
-                response.EnsureSuccessStatusCode();
+                    if (_start)
+                    {
+                        Device.BeginInvokeOnMainThread(() =>
+                        {
+                            Timer = DateTime.Now.Subtract(_startTime).Seconds.ToString();
+                        });
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    
+                });
+            }
+            else
+            {
+                _start = false;
+                AddTimeRequest addTimeRequest = new AddTimeRequest();
+                addTimeRequest.StartTime = _startTime;
+                addTimeRequest.EndTime = DateTime.Now;
 
-                if (response.IsSuccessStatusCode)
+                string json = JsonConvert.SerializeObject(addTimeRequest, Formatting.Indented);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                try
                 {
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    var parsedObject = JObject.Parse(responseBody);
-                    TimeItem item = JsonConvert.DeserializeObject<TimeItem>(responseBody);
-                    _task.Times.Add(item);
-                    Times = convertList(_task, _task.Times);
-                    Debug.WriteLine(response);
+                    Uri uri = new Uri((Urls.HOST + "/" + Urls.ADD_TIME).Replace("{projectId}", _projectId.ToString()).Replace("{taskId}", _Id.ToString()));
+                    if (!client.DefaultRequestHeaders.Contains("Authorization"))
+                    {
+                        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Preferences.Get("access_token", "undefiend"));
+                    }
+                    HttpResponseMessage response = await client.PostAsync(uri, content);
+                    response.EnsureSuccessStatusCode();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var parsedObject = JObject.Parse(responseBody);
+                        TimeItem item = JsonConvert.DeserializeObject<TimeItem>(parsedObject["data"].ToString());
+                        Debug.WriteLine("AAAAAAAAAAAAAAAAAA "+item.Difference);
+                        _task.Times.Add(item);
+                        Times = convertList(_task, _task.Times);
+                        Debug.WriteLine(response);
+                    }
+
+
                 }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                NameButton = "Démarrer";
+                Timer = "";
                 
-
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-
+            
         }
     }
 }
