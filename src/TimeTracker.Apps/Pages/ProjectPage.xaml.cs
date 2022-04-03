b4bdb4abb0a958d85b80d44c22fc770257076b56
Yohann <yohann.baudet@etu.univ-orleans.fr>
2022-04-03 23:26:24 +0200

    Suppression surbrillance viollette liste tasks

--- src/TimeTracker.Apps/Pages/ProjectPage.xaml.cs
+++ src/TimeTracker.Apps/Pages/ProjectPage.xaml.cs
@@ -20,8 +20,13 @@ namespace TimeTracker.Apps.Pages
         {
             InitializeComponent();
             BindingContext = new ProjectViewModel(tasks, project);
+            taskList.ItemTapped += (object sender, ItemTappedEventArgs e) => {
+                // don't do anything if we just de-selected the row.
+                if (e.Item == null) return;
+                // Deselect the item.
+                if (sender is ListView lv) lv.SelectedItem = null;
+            };
 
-            
         }
     }
 }
